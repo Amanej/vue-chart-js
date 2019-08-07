cf5bf0ae5fa30f8f3b7959403cfd9bdc6cc0053e
Aman <aman.mender@tegningtil3d.no>
2019-08-07 14:26:07 +0200

    Reformatted basic chart to typescript

--- src/components/basicChart/basicChart.vue
+++ src/components/basicChart/basicChart.vue
@@ -2,64 +2,69 @@
     <canvas :id="name" :width="width" :height="width" :ref="name"></canvas>
 </template>
 
-<script>
+<script lang="ts">
 import Chart from 'chart.js';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { GrafDataSet } from './../../types/graph'
 
-export default {
-    props: {
-        name: {
-            type: String,
-            required: true,
-            description: 'A unique name, and it will be used for the elements id',
-        },
-        width: {
-            type: String,
-            default: "400",
-        },
-        height: {
-            type: String,
-            default: "400",
-        },
-        chartType: {
-            type: String,
-            default: 'bar',
-        },
-        labels: {
-            type: Array,
-            required: true,
-        },
-        datasets: {
-            type: Array,
-            required: true,
-        },
-        test: {
-            type: String,
-        }
-    },
-    methods: {
-        renderChart() {
-            const myChart = this.$refs[this.name];
-            const chart = new Chart(myChart, {
-                type: this.chartType,
-                data: {
-                    labels: this.labels,
-                    datasets: this.datasets,
+@Component
+export default class BaseChart extends Vue {
+
+    /**
+     * A unique name, and it will be used for the elements id
+     */
+    @Prop({required: true}) public name: string = ''
+
+    /**
+     * The initial inline width of the canvas HTML element.
+    */
+    @Prop({default: "400"}) public width: string = ''
+
+    /**
+     * The initial inline height of the canvas HTML element.
+    */
+    @Prop({default: "400"}) public height: string = ''
+
+    /**
+     * The initial inline height of the canvas HTML element.
+    */
+    @Prop({default: "bar"}) public chartType: string = ''
+
+    /**
+     * The text of the labels in an ordered manner
+    */
+    @Prop({required: true}) public labels: string[] = []
+
+    /**
+     * The datasets for the graph.
+    */
+    @Prop({required: true}) public datasets: GrafDataSet[] = []
+
+    // Methods
+    public renderChart() {
+        const myChart = this.$refs[this.name];
+        const chart = new Chart(myChart, {
+            type: this.chartType,
+            data: {
+                labels: this.labels,
+                datasets: this.datasets,
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                        },
+                    }],
                 },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true,
-                            },
-                        }],
-                    },
-                    responsive: true,
-                },
-            });
-        }
-    },
-    mounted() {
+                responsive: true,
+            },
+        });
+    }
+    
+    // Lifecycle hooks
+    public mounted() {
         this.renderChart()
-    },
+    }
 };
 </script>
