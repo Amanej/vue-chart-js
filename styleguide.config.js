d9e78df8e0b7d1972a99024512436ff5764fa37e
Aman <aman.mender@tegningtil3d.no>
2019-08-08 12:19:56 +0200

    Added info to package json to export components

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,8 @@ module.exports = {
 	// set your styleguidist configuration here
 	title: 'Default Style Guide',
 	components: 'src/components/**/[A-Z]*.vue',
-	defaultExample: true
+	defaultExample: true,
+	serverPort: 7777,
 	// sections: [
 	//   {
 	//     name: 'First Section',
